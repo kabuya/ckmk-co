733959f809cf78a9b9e66b57b23037cd1a733d65
charly <charly.k@meaweb.com>
2022-02-09 17:46:27 +0100

    Fix error path

--- src/resources/js/dependencies/default-dependancies.js
+++ src/resources/js/dependencies/default-dependancies.js
@@ -3,19 +3,25 @@
  *
  * @type {function(*=): jQuery.fn.init|jQuery}
  */
-if(!window.$) const $ = window.$ = window.jQuery = ((window.$) ? window.$ : require('jquery'));
+if(!window.$) {
+    const $ = window.$ = window.jQuery = ((window.$) ? window.$ : require('jquery'));
+}
 
 /**
  * Require FroalaEditor
  *
  * @type {FroalaEditor|{}}
  */
-if(!window.FroalaEditor) const FroalaEditor = window.FroalaEditor = ((window.FroalaEditor) ? window.FroalaEditor : require('froala-editor/js/froala_editor.pkgd.min'));
+if(!window.FroalaEditor) {
+    const FroalaEditor = window.FroalaEditor = ((window.FroalaEditor) ? window.FroalaEditor : require('froala-editor/js/froala_editor.pkgd.min'));
+}
 
 /**
  * Import Datatables
  */
-if(!window.Datatable) const Datatable = window.Datatable = require('datatables.net');
+if(!window.Datatable) {
+    const Datatable = window.Datatable = require('datatables.net');
+}
 
 /**
  * Types prototype method
