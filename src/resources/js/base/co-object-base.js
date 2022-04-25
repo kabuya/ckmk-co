861b878aeb5dbacd9e040d591b8cb458ec9009e2
ckabuya92 <charly.k@meaweb.com>
2022-04-25 15:15:08 +0200

    new version 25-04-2022 15:15:08

--- src/resources/js/base/co-object-base.js
+++ src/resources/js/base/co-object-base.js
@@ -1049,6 +1049,58 @@ class BaseCO {
     }
 
     /**
+     * @param {jQuery|HTMLFormElement|HTMLElement} form
+     * @return {object}
+     */
+    serializeFormData(form) {
+        if(this.isJQueryDom(form)) form = form.get(0);
+        if(!this.isHtmlDom(form)) return;
+        return Array.from((new FormData(form)).entries())
+            .reduce((data, [field, value]) => {
+                let [_, prefix, keys] = field.match(/^([^\[]+)((?:\[[^\]]*\])*)/);
+
+                if (keys) {
+                    keys = Array.from(keys.matchAll(/\[([^\]]*)\]/g), m => m[1]);
+                    value = this.#updateValueForSerialization(data[prefix], keys, value);
+                }
+                data[prefix] = value;
+                return data;
+            }, {});
+    }
+
+    #updateValueForSerialization(data, keys, value) {
+        if (keys.length === 0) {
+            // Leaf node
+            return (value ? value : null);
+        }
+
+        let key = keys.shift();
+        if (!key) {
+            data = data || [];
+            if (Array.isArray(data)) {
+                key = data.length;
+            }
+        }
+
+        // Try converting key to a numeric value
+        let index = +key;
+        if (!isNaN(index)) {
+            // We have a numeric index, make data a numeric array
+            // This will not work if this is a associative array
+            // with numeric keys
+            data = data || [];
+            key = index;
+        }
+
+        // If none of the above matched, we have an associative array
+        data = data || {};
+
+        data[key] = this.#updateValueForSerialization(data[key], keys, value);;
+
+        return (data ? data : null);
+    }
+
+    /**
      * @param {string} _str
      * @return {string}
      */
